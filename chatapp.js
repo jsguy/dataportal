c08fb91146c99a2289fb01f12def046063630fe3
Mikkel Bergmann <mikkel@jsguy.com>
2016-05-10 12:20:50 +1000

    Update sockjs and make data portal a module

---
+++ chatapp.js
@@ -0,0 +1,20 @@
+var dataportal = require('./lib/dataportal.server.js');
+
+dataportal({
+	createResponse: function(response) {
+		//	Make sure it is a topic we care about
+		if(! (response && response.topic && response.topic == "chatTopic")) {
+			return response;
+		}
+
+		//	You could do something here with the chat data
+		console.log('respond', JSON.stringify(response.diff.chats));
+		return response;
+	},
+	handler: {
+		log: function(severity, line) {
+			//	Skip info
+			return severity !== "info"? console.log(severity, line): null;
+		}
+	}
+});
