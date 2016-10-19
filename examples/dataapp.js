e7c8636b5dd61df29be33564b406cfff683fb222
jsguy <mikkel@pointful.com>
2016-10-20 08:01:27 +1100

    Update sockjs, make patch work better

--- examples/dataapp.js
+++ examples/dataapp.js
@@ -10,17 +10,23 @@ var dp = dataportalServer({
 			data: data
 		}));
 	}
+	,handler: {
+		log: function(severity, line) {
+			//	Skip info
+			return severity !== "info"? console.log(severity, line): null;
+		}
+	}
 });
 
-//	The below creates random names and publishes them
+//	The below creates random names and publishes them at randomish times.
 var rnd = function(list) {
 	var i = Math.floor(Math.random() * list.length);
 		return list[i];
 	},
 	adjectives = ["adamant", "adroit", "amatory", "animistic", "antic", "arcadian", "baleful", "bellicose", "bilious", "boorish", "calamitous", "caustic", "cerulean", "comely", "concomitant", "contumacious", "corpulent", "crapulous", "defamatory", "didactic", "dilatory", "dowdy", "efficacious", "effulgent", "egregious", "endemic", "equanimous", "execrable", "fastidious", "feckless", "fecund", "friable", "fulsome", "garrulous", "guileless", "gustatory", "heuristic", "histrionic", "hubristic", "incendiary", "insidious", "insolent", "intransigent", "inveterate", "invidious", "irksome", "jejune", "jocular", "judicious", "lachrymose", "limpid", "loquacious", "luminous", "mannered", "mendacious", "meretricious", "minatory", "mordant", "munificent", "nefarious", "noxious", "obtuse", "parsimonious", "pendulous", "pernicious", "pervasive", "petulant", "platitudinous", "precipitate", "propitious", "puckish", "querulous", "quiescent", "rebarbative", "recalcitant", "redolent", "rhadamanthine", "risible", "ruminative", "sagacious", "salubrious", "sartorial", "sclerotic", "serpentine", "spasmodic", "strident", "taciturn", "tenacious", "tremulous", "trenchant", "turbulent", "turgid", "ubiquitous", "uxorious", "verdant", "voluble", "voracious", "wheedling", "withering", "zealous"];
 	nouns = ["ninja", "chair", "pancake", "statue", "unicorn", "rainbows", "laser", "senor", "bunny", "captain", "nibblets", "cupcake", "carrot", "gnomes", "glitter", "potato", "salad", "toejam", "curtains", "beets", "toilet", "exorcism", "stick figures", "mermaid eggs", "sea barnacles", "dragons", "jellybeans", "snakes", "dolls", "bushes", "cookies", "apples", "ice cream", "ukulele", "kazoo", "banjo", "opera singer", "circus", "trampoline", "carousel", "carnival", "locomotive", "hot air balloon", "praying mantis", "animator", "artisan", "artist", "colorist", "inker", "coppersmith", "director", "designer", "flatter", "stylist", "leadman", "limner", "make-up artist", "model", "musician", "penciller", "producer", "scenographer", "set decorator", "silversmith", "teacher", "auto mechanic", "beader", "bobbin boy", "clerk of the chapel", "filling station attendant", "foreman", "maintenance engineering", "mechanic", "miller", "moldmaker", "panel beater", "patternmaker", "plant operator", "plumber", "sawfiler", "shop foreman", "soaper", "stationary engineer", "wheelwright", "woodworkers"],
-	minTime = 500,
-	maxTime = 2000,
+	minTime = 5000,
+	maxTime = 5000,
 	rndTime = function(){
 		return Math.floor(Math.random() * maxTime) + minTime;
 	},
@@ -31,7 +37,11 @@ var rnd = function(list) {
 			dataRow = {
 				name: name,
 				text: "Hello, I'm " + name,
-				time: timeObj.getTime()
+				time: (
+					timeObj.getHours() + ":" + 
+					(timeObj.getMinutes()<10?'0':'') + timeObj.getMinutes() + ":" + 
+					(timeObj.getSeconds()<10?'0':'') + timeObj.getSeconds()
+				)
 			};
 		return dataRow;
 	},
@@ -63,10 +73,7 @@ var modifyData = function(){
 	var diff = jdp.diff(oData, data);
 
 	if(diff) {
-		//console.log("Diff", diff);
 		dp.publish(myTopic, {diff: diff});
-	} else {
-		console.warn("No diff?", data);
 	}
 
 	setTimeout(modifyData, rndTime());
